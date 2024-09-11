83670d6045bcf8b04cea7e0a237053b608679641
Hashim Anhad <anshadanchu368@gmail.com>
2024-09-11 13:01:48 +0530

    3dscene

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import React ,{useState} from "react";
-import useCurrencyInfo from "../../converter/src/hooks/useCurrency";
+import React ,{useState,Suspense} from "react";
+import {Canvas} from "@react-three/fiber"
+import {OrbitControls,Preload,useGLTF } from "@react-three/drei"
+import EarthCanvas from "./components/Earth";
+
+
 
 const App = () => {
     const [text,setText]=useState("");
@@ -7,41 +11,39 @@ const App = () => {
     const [to,setTo]=useState("ar")
     const [result,setResult]=useState("")
 
-    const languages= useCurrencyInfo(from);
 
-    const {translatedText}=Object.keys(languages)
+ 
 
-    const swap=()=>{
-       setFrom(to)
-       setTo(from)
-       setResult(text)
-       setText(result)
-    }
-
-    const translate=()=>{
-       setResult(translatedText)
-    }
 
 
   return (
     <div className="min-h-screen w-full bg-slate-900  flex justify-center items-center ">
-      <div className="flex justify-center items-center min-h-[50vh] w-[70vw] bg-slate-900  rounded-lg relative">
-        <div className="flex w-[50vw] ">
-          <div className="w-1/2 bg-slate-800 h-[40vh] max-h-[40vh] rounded-lg overflow-auto hove:ring-cyan-700 hover:ring-4 ">
-               
-          
+      <div className="flex justify-center items-center min-h-[100vh] w-[70vw] bg-slate-900  rounded-lg relative">
+        
+
+        <div className="flex w-[50vw]">
+          <div className="w-1/2 bg-slate-800/40 h-[40vh] max-h-[40vh] rounded-lg overflow-auto hove:ring-cyan-700 hover:ring-4 z-20">
+             
+              
           </div>
-          <div className="w-1/2 bg-slate-700 h-[40vh] max-h-[40vh] rounded-lg overflow-auto hover:ring-cyan-700 hover:ring-4">
+           
+
+          <div className="w-1/2 bg-slate-700/40 h-[40vh] max-h-[40vh] rounded-lg overflow-auto hover:ring-cyan-700 hover:ring-4 z-20">
           
           
           </div>
         </div>
-        <button className="bg-blue-600 px-3 py-5 font-semibold text-black absolute  shadow-md rounded-lg hover:bg-slate-950 hover:text-white" onClick={()=>{translatedText}}>
+        <button className="bg-blue-600 px-3 py-5 font-semibold text-black absolute  shadow-md rounded-lg hover:bg-slate-950 hover:text-white z-30" >
           Swap
         </button>
+        <div className="absolute w-[90vw] h-[90vh]">
+          <EarthCanvas/>
+        </div>
       </div>
     </div>
   );
 };
+
+
 
 export default App;
